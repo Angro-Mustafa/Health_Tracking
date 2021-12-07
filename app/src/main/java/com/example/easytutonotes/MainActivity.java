4534899d4cc252f76ef472f58463a2b486ac5d78
Angro-Mustafa <mustafaangro94@gmai.com>
2021-12-07 23:32:20 +0200

    Gluocose part

---
+++ app/src/main/java/com/example/easytutonotes/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.easytutonotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import io.realm.Realm;
+import io.realm.RealmChangeListener;
+import io.realm.RealmResults;
+import io.realm.Sort;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        MaterialButton addNoteBtn = findViewById(R.id.addnewnotebtn);
+
+        addNoteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,AddGlocoseMeasurment.class));
+            }
+        });
+
+        Realm.init(getApplicationContext());
+        Realm realm = Realm.getDefaultInstance();
+
+        RealmResults<Measure> notesList = realm.where(Measure.class).findAllSorted("createdTime", Sort.DESCENDING);
+
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        MyAdapter myAdapter = new MyAdapter(getApplicationContext(),notesList);
+        recyclerView.setAdapter(myAdapter);
+
+        notesList.addChangeListener(new RealmChangeListener<RealmResults<Measure>>() {
+            @Override
+            public void onChange(RealmResults<Measure> measures) {
+                myAdapter.notifyDataSetChanged();
+            }
+        });
+
+
+    }
+}
